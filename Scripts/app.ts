71982cdc129e5a3a3fbc775b41c19f3e41ba7ef2
Minah Cho <19972001@gmail.com>
2022-10-16 22:22:29 -0400

    deploy live server and try to do app.ts

---
+++ Scripts/app.ts
@@ -0,0 +1,13 @@
+"use strict";
+
+const message: string = 'hello wrold';
+console.log(message);
+
+// bio
+// primitive tyes
+let name: string = "minah";
+let age: number = 25;
+let phone: string = "000-000-0000";
+let mission: string[] = ["yoga", "reading books", "studying Javascript"];
+
+//co
